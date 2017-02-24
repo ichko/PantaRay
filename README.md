b88fccb4bd09bb45355f11ec1196e637ba6b9970
Iliya Zhechev <i4ko95@gmail.com>
2017-02-24 11:25:24 +0200

    SDL example image

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ C++ Raytracer
 
 ###Console Example
 ![console example](assets/console_example.png)
+
+###Console Example
+![console example](assets/sdl_example.png)
--- pantaray/main.cpp
+++ pantaray/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
         .Setup(Vector(10, 0, 20), Vector(10, 1, 20), Vector(0, 0, 1));
 
     auto plane_geometry = PlaneGeometry(Vector(), Vector(0.3, 0.3, 1));
-    auto sphere_geometry = SphereGeometry(Vector(30, 100, 30), 20);
+    auto sphere_geometry = SphereGeometry(Vector(30, 50, 30), 20);
 
     auto checker_shader = CheckerShader(Color(1, 1, 0), Color(0, 1, 0));
     auto sphere_checker_shader = CheckerShader(Color(1, 0, 0), Color(0, 0, 1), 0.0001);
