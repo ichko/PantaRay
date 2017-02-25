beba481810258fb3733430bb544f23ebabc530f1
Iliya Zhechev <i4ko95@gmail.com>
2017-02-25 14:42:23 +0200

    Example

--- pantaray/main.cpp
+++ pantaray/main.cpp
@@ -11,25 +11,56 @@ int main(int argc, char** argv) {
     auto width = 500;
     auto height = 500;
 
-    auto renderer = Renderer(width, height);
+    auto renderer = Renderer(width, height, Color(0.01, 0.01, 0.01));
+    // renderer.anti_aliasing = true;
 
     auto camera = PinholeCamera(pi / 2.5, width / height)
         .Set(Vector(10, 0, 20), Vector(10, 1, 20), Vector(0, 0, 1));
 
-    auto plane_geometry = PlaneGeometry(Vector(0, 0, 0), Vector(0, 0, 1));
-    auto sphere_geometry = SphereGeometry(Vector(30, 80, 30), 20);
+    auto plane_geometry_top = PlaneGeometry(Vector(0, 0, 0), Vector(0, 0, 1));
+    auto plane_geometry_bottom = PlaneGeometry(Vector(0, 0, 60), Vector(0, 0, -1));
+    auto plane_geometry_left = PlaneGeometry(Vector(-20, 0, 0), Vector(1, 0, 0));
+    auto plane_geometry_back = PlaneGeometry(Vector(0, 100, 0), Vector(0, -1, 0));
+    auto plane_geometry_right = PlaneGeometry(Vector(50, 0, 0), Vector(-1, 0, 0));
 
-    auto checker_shader = CheckerShader(Color(1, 1, 0), Color(0, 1, 0));
+
+    auto sphere_geometry = SphereGeometry(Vector(35, 80, 30), 20);
+    auto sphere_geometry_small = SphereGeometry(Vector(12, 75, 30), 12);
+    auto sphere_geometry_big = SphereGeometry(Vector(-10, 75, 0), 25);
+
+    auto checker_shader = CheckerShader(Color(0.8, 0.8, 0.8), Color(0.1, 0.1, 0.1));
     auto sphere_checker_shader = CheckerShader(Color(1, 0, 0), Color(0, 0, 1), 0.01f);
     auto normal_shader = NormalShader();
-    auto lambert_shader = LambertShader(Color(1, 0, 0));
-    auto point_light = PointLight(Vector(-10, 50, 20), 422);
+
+    auto lambert_shader_red = LambertShader(Color(1, 0, 0));
+    auto lambert_shader = LambertShader(Color(1, 1, 1));
+
+    auto composed_shader = CopositionShader(checker_shader, lambert_shader_red);
+
+    auto point_light_red = PointLight(Vector(10, 40, 25), 550, Color(1, 0, 0));
+    auto point_light_green = PointLight(Vector(15, 40, 40), 500, Color(0, 1, 0));
+    auto point_light_blue = PointLight(Vector(30, 40, 30), 500, Color(0, 0, 1));
+    auto point_light_big = PointLight(Vector(30, 80, 55), 400, Color(1, 1, 1));
+
+    auto ambient_light = AmbientLight(Color(0.01, 0.01, 0.01));
 
     auto scene = Scene()
-        .Add(Mesh(plane_geometry, lambert_shader))
-        .Add(Mesh(sphere_geometry, normal_shader))
+        .Add(Mesh(plane_geometry_top, composed_shader))
+        .Add(Mesh(plane_geometry_bottom, composed_shader))
+        .Add(Mesh(plane_geometry_left, lambert_shader_red))
+        .Add(Mesh(plane_geometry_back, lambert_shader_red))
+        .Add(Mesh(plane_geometry_right, lambert_shader_red))
 
-        .Add(point_light);
+        .Add(Mesh(sphere_geometry, lambert_shader))
+        .Add(Mesh(sphere_geometry_small, lambert_shader))
+        .Add(Mesh(sphere_geometry_big, lambert_shader))
+
+        .Add(point_light_red)
+        .Add(point_light_green)
+        .Add(point_light_blue)
+        //.Add(point_light_big)
+
+        .Add(ambient_light);
 
     auto buffer = renderer.Render(camera, scene);
 
